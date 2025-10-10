88507971fd573f32c34f83bcb290bc5a50465bdc
SF4524LogeshKumar <logeshkumar.dhamodharan@syncfusion.com>
2025-10-10 14:50:54 +0530

    940507: Sample on How to add Form Design Sign in Default Toolbar

---
+++ How to/Form Designer Signature in Default Toolbar/WebApplication5/WebApplication5/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Syncfusion.EJ2.PdfViewer;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection;
+using Syncfusion.Pdf.Parsing;
+//using SkiaSharp;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Pdf.Security;
+using Syncfusion.Pdf;
+using System.Security.Cryptography.X509Certificates;
+using Syncfusion.Drawing;
+using System.IO;
+using Syncfusion.Pdf.Interactive;
+using Syncfusion.Pdf.Redaction;
+using System.Text.Json.Nodes;
+using SkiaSharp;
+
+namespace WebApplication5.Pages
+{
+    [IgnoreAntiforgeryToken(Order = 1001)]
+    public class IndexModel : PageModel
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        private IMemoryCache _cache;
+        public IndexModel(IWebHostEnvironment hostingEnvironment, IMemoryCache cache)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _cache = cache;
+        }
+
+        public class CustomToolbarItems
+        {
+            public CustomToolbarItems() { }
+            public List<object> ToolbarItems { get; set; }
+
+        }
+    }
+}
