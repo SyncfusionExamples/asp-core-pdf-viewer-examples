a0e3d9e0223444fe7d859f760be4ba741181f667
Deepika <deepika.ravi@syncfusion.com>
2026-04-07 15:45:59 +0530

    Added the sample for signature video

---
+++ Videos/Siganture/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Siganture.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
